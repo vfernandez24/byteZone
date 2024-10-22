3a7b705b0a8354e5423917abaec24548e2207b68
Victor Fernandez <germanfernandezblanco@gmail.com>
2024-10-22 17:18:21 +0200

    e

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,6 @@
 console.log('filter.js conectado')
 
 function toggleMenu() {
-    //????????
+    document.getElementById('aside').classList.toggle('close')
+    document.getElementById('buttonAside').classList.toggle('rotate')
 }
