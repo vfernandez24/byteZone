68b3474b4086f4ae7fdb598afe4aae09ad8f1d60
Victor Fernandez <germanfernandezblanco@gmail.com>
2024-10-29 15:52:37 +0100

    e

--- js/scrollSection.js
+++ js/scrollSection.js
@@ -1,9 +1,25 @@
 function sectionLeft(btnLeft) {
-    let section = btnLeft.parentElement;
-    section.scrollLeft -= 260;
+    let sectionContainer = btnLeft.parentElement;
+    let section = sectionContainer.querySelector('.section');
+    section.scrollLeft -= 265;
+
+    if (section.scrollLeft <= 265) {
+        btnLeft.classList.add('hidden')
+    }
 }
 
 function sectionRight(btnRight) {
-    let section = btnRight.parentElement
-    section.scrollLeft += 260; 
+    let sectionContainer = btnRight.parentElement;
+
+    let section = sectionContainer.querySelector(".section");
+    section.scrollLeft += 265; 
+    
+    let btnLeft = sectionContainer.querySelector('.btnLeft')
+    btnLeft.classList.remove('hidden')
+
+    if (section.scrollLeft + section.clientWidth >= section.scrollWidth) {
+        section.scrollLeft = 0;
+        btnLeft.classList.add('hidden')
+    }
+
 }
