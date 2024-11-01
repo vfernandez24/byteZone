1d091deef3213c499e5ec1921f03faa7213e03dc
Victor Fernandez <germanfernandezblanco@gmail.com>
2024-11-01 17:34:59 +0100

    e

--- js/cesta.js
+++ js/cesta.js
@@ -2,12 +2,12 @@ console.log('cesta.js conectado')
 
 const showCesta = () => {
     document.getElementById("body").style.overflowY = "hidden";
-    document.getElementById("overlayCesta").classList.add("visible");
+    document.getElementById("overlay").classList.add("visible");
     document.getElementById("cesta").classList.add("visible");
 };
 
 const closeCesta = () => {
     document.getElementById('body').style.overflowY = 'auto'
-    document.getElementById('overlayCesta').classList.remove('visible')
+    document.getElementById('overlay').classList.remove('visible')
     document.getElementById('cesta').classList.remove('visible')
 }
