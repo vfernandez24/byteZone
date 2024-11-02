5a68f1a7e71ca0d9af8ee7dfbfa772a9e117eb2c
Victor Fernandez <germanfernandezblanco@gmail.com>
2024-11-02 18:02:04 +0100

    e

--- js/perfil.js
+++ js/perfil.js
@@ -1,13 +1,22 @@
 console.log("perfil.js conectado");
 
+let clickPerfil = 0
+// clickPerfil = localStorage.getItem('clickPerfil')
+
 const showPerfil = () => {
     document.getElementById("body").style.overflowY = "hidden";
     document.getElementById("overlay").classList.add("visible");
     document.getElementById("perfil").classList.add("visible");
+    clickPerfil++;
+    localStorage.setItem('clickPerfil', clickPerfil)
 };
 
 const closePerfil = () => {
     document.getElementById("body").style.overflowY = "auto";
     document.getElementById("overlay").classList.remove("visible");
     document.getElementById("perfil").classList.remove("visible");
+};
+
+const paginaPerfil = () => {
+
 };
