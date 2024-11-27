96ab164f8e2b496190cadf5318c46234b782850b
Victor Fernandez <germanfernandezblanco@gmail.com>
2024-11-27 17:35:48 +0100

    e

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Creando el responsive (width: <= 900px) => style.css, createBase.js
+    Hay que cambiar en las páginas la dirección de las fotos del nav
+    Hay que ajustar el footer
--- js/left.js
+++ js/left.js
@@ -5,3 +5,23 @@ const toggleLeft = () => {
     arrow.classList.toggle('fa-chevron-left');
     arrow.classList.toggle('fa-chevron-right');
 }
+
+// Seleccionar el elemento al que deseas añadir/eliminar la clase
+const element = document.querySelector('#left');
+
+// Crear un MediaQueryList para el ancho máximo de 900px
+const mediaQuery = window.matchMedia('(max-width: 900px)');
+
+// Función para manejar los cambios en la media query
+function handleMediaQueryChange(e) {
+    if (e.matches) {
+        // Si el dispositivo tiene un ancho menor o igual a 900px
+        toggleLeft();
+    }
+}
+
+// Agregar un listener para cambios en la media query
+mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+// Ejecutar la función al cargar la página para manejar el estado inicial
+handleMediaQueryChange(mediaQuery);
